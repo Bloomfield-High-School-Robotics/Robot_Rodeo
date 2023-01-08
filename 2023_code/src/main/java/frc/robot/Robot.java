a6bb40bbe08fe0e45aee86e0af690dfd5cbdd363
Dave Persaud <davep218@outlook.com>
2023-01-07 20:07:19 -0500

    second push
    line up to target function that finds the inputs to turn and move forward to the target (currently set for arcade drive)

--- 2023_code/src/main/java/frc/robot/Robot.java
+++ 2023_code/src/main/java/frc/robot/Robot.java
@@ -8,7 +8,9 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Robot extends TimedRobot {
   //private vars
-
+  private boolean m_LimelightHasValidTarget = false;
+  private double m_LimelightDriveCommand = 0.0;
+  private double m_LimelightSteerCommand = 0.0;
    @Override
   public void robotInit() {
 
@@ -43,6 +45,41 @@ public class Robot extends TimedRobot {
   ret[2]=area;
   ret[3]=v;
   return ret;
+  }
+  public void lineToTarget(){
+        // These numbers must be tuned for your Robot!  Be careful!
+    final double STEER_K = 0.03;                    // how hard to turn toward the target
+    final double DRIVE_K = 0.26;                    // how hard to drive fwd toward the target
+    final double DESIRED_TARGET_AREA = 13.0;        // Area of the target when the robot reaches the wall
+    final double MAX_DRIVE = 0.7;                   // Simple speed limit so we don't drive too fast
+
+    double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+    double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+
+    if (tv < 1.0)
+    {
+      m_LimelightHasValidTarget = false;
+      m_LimelightDriveCommand = 0.0;
+      m_LimelightSteerCommand = 0.0;
+      return;
+    }
+
+    m_LimelightHasValidTarget = true;
+
+    // Start with proportional steering
+    double steer_cmd = tx * STEER_K;
+    m_LimelightSteerCommand = steer_cmd;
+
+    // try to drive forward until the target area reaches our desired area
+    double drive_cmd = (DESIRED_TARGET_AREA - ta) * DRIVE_K;
+
+    // don't let the robot drive too fast into the goal
+    if (drive_cmd > MAX_DRIVE)
+    {
+      drive_cmd = MAX_DRIVE;
+    }
+    m_LimelightDriveCommand = drive_cmd;
   }
     @Override
     public void autonomousInit() {
