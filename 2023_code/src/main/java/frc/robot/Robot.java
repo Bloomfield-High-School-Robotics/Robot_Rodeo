45126f6255d4c960051c0cfed1a51da0307e93b7
herefornow12 <63117624+herefornow12@users.noreply.github.com>
2023-01-31 15:24:53 -0500

    Update Robot.java

--- 2023_code/src/main/java/frc/robot/Robot.java
+++ 2023_code/src/main/java/frc/robot/Robot.java
@@ -1,57 +1,154 @@
-
 package frc.robot;
+
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import com.revrobotics.*;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.networktables.*;
+import java.math.*;
+//import com.ctre.phoenix.sensors.Pigeon2;
+
+
+
 
 public class Robot extends TimedRobot {
-  //private vars
+  private XboxController control1;
+  private XboxController control2;
+  private CANSparkMax dt_ll;
+  private CANSparkMax dt_lf;
+  private CANSparkMax dt_lf1;
+  private CANSparkMax dt_rf;
+  private CANSparkMax dt_rf1;
+  private CANSparkMax dt_rl;
+  private MotorControllerGroup mc_left;
+  private MotorControllerGroup mc_right;
+  private DifferentialDrive dt_main;
+  private Double errorAdjust;
+  //private Pigeon2 pig;
+  private RelativeEncoder dt_enc_1;
+  private final double kEncConstant = Math.PI * 6 / 8.46;
+  private final double kP = 0.07;
+  private int setpoint;
+  private double error;
+  private SlewRateLimiter limiter;
 
-   @Override
+
+ @Override
   public void robotInit() {
+    //pid = new PIDController(5, 0, 15);
 
-  }
+
+    //pig = new Pigeon2(7);
+
+
+    dt_ll = new CANSparkMax(4, MotorType.kBrushless);
+    dt_lf = new CANSparkMax(6, MotorType.kBrushless);
+    dt_lf1 = new CANSparkMax(5, MotorType.kBrushless);
+
+
+    dt_rl = new CANSparkMax(1, MotorType.kBrushless);
+    dt_rf = new CANSparkMax(2, MotorType.kBrushless);
+    dt_rf1 = new CANSparkMax(3, MotorType.kBrushless);
+
+
+    control1 = new XboxController(1);
+    //control2 = new XboxController(2);
+
+
+    dt_enc_1 = dt_ll.getEncoder();
+
+
+    mc_left = new MotorControllerGroup(dt_ll, dt_lf, dt_lf1);
+    mc_right  = new MotorControllerGroup(dt_rl, dt_rf, dt_rf1);
+    dt_main = new DifferentialDrive(mc_left, mc_right);  
+    mc_left.setInverted(true);
+
+
+    limiter = new SlewRateLimiter(0.33);
+    getTelem();
+   }
+
+
   @Override
   public void teleopPeriodic() {
-    //limeVals is an array of doubles length of 3. first is the x value read from the limelight, second is the y value, and 3rd is area
-    Double[] limeVals=limelightPeriodic();
-    
-  }
-  public Double[] limelightPeriodic(){
-  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-  NetworkTableEntry tx = table.getEntry("tx");
-  NetworkTableEntry ty = table.getEntry("ty");
-  NetworkTableEntry ta = table.getEntry("ta");
-  NetworkTableEntry tv = table.getEntry("tv");
-
-  //read values periodically
-  double x = tx.getDouble(0.0);
-  double y = ty.getDouble(0.0);
-  double area = ta.getDouble(0.0);
-  double v = tv.getDouble(0.0);
-
-  //post to smart dashboard periodically
-  SmartDashboard.putNumber("LimelightX", x);
-  SmartDashboard.putNumber("LimelightY", y);
-  SmartDashboard.putNumber("LimelightArea", area);
-  SmartDashboard.putNumber("LimelightValidTarget",v);
-  Double[] ret = new Double[4];
-  ret[0]=x;
-  ret[1]=y;
-  ret[2]=area;
-  ret[3]=v;
-  return ret;
-  }
-    @Override
-    public void autonomousInit() {
-  
+    getTelem();
+    getTurn();
+    if(control1.getBButton()) {
+    dt_main.tankDrive(-errorAdjust, errorAdjust);
     }
+    else {
+      dt_main.tankDrive(limiter.calculate(control1.getLeftY()), limiter.calculate(control1.getRightY()));
+    }
+  }
+ 
+  @Override
+  public void autonomousInit() {
+    dt_enc_1.setPosition(0.0);
+    getTelem();
+  }
+
 
   @Override
   public void autonomousPeriodic() {
+    setpoint = 50;
+    double feet = ((dt_enc_1.getPosition()*kEncConstant)/12);
+    error = setpoint - feet;
+    double output = kP * error;
+    if (error <= 1) {
+      dt_main.tankDrive(0, 0);
+    }
+    if(Math.abs(output) > 1){
+      dt_main.tankDrive(-0.75, -0.75);
+    }
+    else{
+      dt_main.tankDrive(-output*.75, -output*.75);
+    }
+    getTelem();
+  }
 
+
+  public void getTurn(){
+    final double constant = .025;
+    errorAdjust = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0) * constant;
+  }
+
+
+  @Override
+  public void disabledInit() {
+    dt_enc_1.setPosition(0.0);
+    getTelem();
+  }
+  //Gets telemetry values
+  public void getTelem() {    
+    SmartDashboard.putNumber("Left Y-axis", control1.getLeftY());
+    SmartDashboard.putNumber("Right Y-axis", control1.getRightY());
+    SmartDashboard.putNumber("Right Trigger", control1.getRightTriggerAxis());
+    SmartDashboard.putNumber("Left Trigger", control1.getLeftTriggerAxis());
+    SmartDashboard.putNumber("tx",NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0));
+    SmartDashboard.putNumber("ty",NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0));
+    //SmartDashboard.putNumber("ta",NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0));
+    SmartDashboard.putNumber("Error tx",NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0));
+    double feet = (dt_enc_1.getPosition()*kEncConstant)/12;
+    double rpm = dt_enc_1.getVelocity();
+    SmartDashboard.putNumber("Encoder rpm", rpm);
+    SmartDashboard.putNumber("Encoder feet", feet);
+    SmartDashboard.putNumber("Error for Auton PID", error);
+    SmartDashboard.putNumber("Setpoint", setpoint);
+    //SmartDashboard.putNumber("Yaw", pig.getYaw());
+    //SmartDashboard.putNumber("Pitch", pig.getPitch());
+    //SmartDashboard.putNumber("Roll", pig.getRoll());
   }
 }
+
+
 
